f779544e37fb43c1c9be060d5cb15c68bf15d32a
AamaeyPrakash <aamaeyprakash@gmail.com>
2025-05-29 16:57:08 +0530

    num plate detection

--- numplate_detection.py
+++ numplate_detection.py
@@ -1,20 +1,28 @@
 import cv2
 import pytesseract
 
-img = cv2.imread("./assets/numplate1.png") 
-text = pytesseract.image_to_string(img)
+img = cv2.imread("./assets/numplate2.jpg")
+
+plateHC = cv2.CascadeClassifier("haarcascade_russian_plate_number.xml")
+
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray,(5,5),1.4)
 edge = cv2.Canny(blur,50,150)
-
-text2 = pytesseract.image_to_string(edge)
+num_plates = plateHC.detectMultiScale(gray)
+for x,y,w,h in num_plates:
+    cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    detected_plate1 = gray[y : y+h, x : x+w]
+    detected_plate2 = edge[y : y+h, x : x+w]
+    text_plate1 = pytesseract.image_to_string(detected_plate1)
+    text_plate2 = pytesseract.image_to_string(detected_plate2)
+    print("Detected Text:", text_plate1)
+    print("Detected Text:", text_plate2)
 
 cv2.imshow("Original Image", img)
-cv2.imshow("Blur + Edge", edge)
+cv2.imshow("Gray Plate", detected_plate1)
+cv2.imshow("Edge Plate", detected_plate2)
 
-print("Detected Text:", text)
-print("Detected Text 2:", text2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
